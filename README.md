13261b8f40228aa369bb4ed0cbf64003af49daca
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-29 17:18:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fsd5-group11-merry-match
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex flex-col justify-center items-center text-3xl font-bold">
+        <h1>App</h1>
+        <h1>Apply Tailwind CSS Already</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
