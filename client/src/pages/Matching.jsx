6f3ebcf5b769de16201c36bd9d7c8bd3b1a04d53
Kaew <rajitkaew2285@gmail.com>
2024-01-30 19:00:14 +0700

    feat:add components navbar

--- client/src/pages/Matching.jsx
+++ client/src/pages/Matching.jsx
@@ -1,7 +1,13 @@
 import MerryCards from "../components/MerryCards";
-
+import NavBar from "../components/NavBar";
 function Matching() {
-  return <MerryCards />;
+  return (
+    <>
+      <NavBar />
+
+      <MerryCards />
+    </>
+  );
 }
 
 export default Matching;
