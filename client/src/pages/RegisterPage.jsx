fee65a2a2af2d01520b7ce30de27a7597c2be03a
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-30 14:45:14 +0700

    fix: resolve rebase conflict

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useForm } from "react-hook-form";
 import InputSelect from "../components/common/InputSelect";
 
@@ -86,6 +87,60 @@ const RegisterPage = () => {
           {errors.password && <span>This field is required</span>}
         </div>
         <input type="submit" />
+=======
+const RegisterPage = () => {
+  return (
+    <div className="register-form-container">
+      <form className="register-form">
+        <h1>Register Form</h1>
+        <div className="input-container">
+          <label>
+            Username
+            <input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Enter username here"
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Password
+            <input
+              id="password"
+              name="password"
+              type="text"
+              placeholder="Enter password here"
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            First Name
+            <input
+              id="firstname"
+              name="firstname"
+              type="text"
+              placeholder="Enter first name here"
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Last Name
+            <input
+              id="lastname"
+              name="lastname"
+              type="text"
+              placeholder="Enter last name here"
+            />
+          </label>
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+        </div>
+>>>>>>> bd08fb3 (fix: add register page)
       </form>
     </div>
   );
