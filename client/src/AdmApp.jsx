7a0ab829c025b36db5713b9d2be25a3e81d36df9
devsomsak <devrleang@gmail.com>
2024-02-01 14:52:06 +0700

    fix: conflict

---
+++ client/src/AdmApp.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./utils/supabaseClient";
+import Auth from "./AdmAuth";
+import Account from "./AdmAccount";
+
+function AdmApp() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  );
+}
+
+export default AdmApp;
