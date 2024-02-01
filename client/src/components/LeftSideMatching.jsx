115137502cb093b1f119e3ba3fd16997b5c48f8a
Kaew <rajitkaew2285@gmail.com>
2024-02-01 10:43:37 +0700

    feat:add components slider age

--- client/src/components/LeftSideMatching.jsx
+++ client/src/components/LeftSideMatching.jsx
@@ -1,18 +1,38 @@
 import seach from "../assets/Matching/search.svg";
+import person1 from "../assets/Matching/person1.svg";
+import person2 from "../assets/Matching/person2.svg";
+import person3 from "../assets/Matching/person3.svg";
 const LeftSideMatching = () => {
   return (
-    <div className="w-[314px] h-[259px] flex flex-col justify-center items-center ml-10">
-      <div className=" flex  flex-col justify-center items-center w-[282px] h-[187px] border-[#A62D82] border rounded-[16px]">
-        <img className="w-[55px] h-[55px]" src={seach} />
-        <h1 className="text-xl font-bold text-[#95002B]">Discover New Match</h1>
-        <p className="text-xs h-[42px] w-[234px] text-center">
-          Start find and Merry to get know and connect with new friend!.
-        </p>
+    <div className="w-[314px] h-[500px] flex-col flex mr-5 ">
+      <div className=" flex flex-col items-center  mt-7 ">
+        <div className=" flex  flex-col justify-center items-center w-[282px] h-[187px] border-[#A62D82] border rounded-[16px]">
+          <img className="w-[55px] h-[55px]" src={seach} />
+          <h1 className="text-xl font-bold text-[#95002B]">
+            Discover New Match
+          </h1>
+          <p className="text-xs h-[42px] w-[234px] text-center">
+            Start find and Merry to get know and connect with new friend!.
+          </p>
+        </div>
       </div>
-      <h1>Merry Match!</h1>
-      <h1>Chat with Merry Match</h1>
-      <p>Ygritte</p>
-      <p>You know nothing Jon Snow</p>
+      <div className="mt-10 ml-5">
+        <h1 className="text-xl font-bold">Merry Match!</h1>
+        <div className="flex flex-row mt-3 ">
+          <img className="w-[65px] h-[65px]" src={person1} />
+          <img className="w-[65px] h-[65px] ml-2" src={person2} />
+        </div>
+      </div>
+      <div className="mt-5 ml-5">
+        <h1 className="text-xl font-bold">Chat with Merry Match</h1>
+        <div className="flex flex-row mt-3">
+          <img className="w-[50px] h-[50px]" src={person3} />
+          <div className="ml-3">
+            <p>Ygritte</p>
+            <p>You know nothing Jon Snow</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Matching.jsx
+++ client/src/pages/Matching.jsx
@@ -1,13 +1,36 @@
 import MerryCards from "../components/MerryCards";
 import NavBar from "../components/NavBar";
 import LeftSideMatching from "../components/LeftSideMatching";
+import { Checkbox } from "@chakra-ui/react";
+import SliderAge from "../components/SliderAge";
+
 function Matching() {
   return (
     <>
       <NavBar />
-      <div className="flex flex-row mt-10 ">
+      <div className="flex flex-row mt-10 ml-5 ">
         <LeftSideMatching />
         <MerryCards />
+        <div className="w-[314px] h-[500px] flex-col flex ml-[18px] ">
+          <p className=" text-[#191C77] text-base font-bold">
+            Sex you interest
+          </p>
+          <div className="mt-2 flex flex-col">
+            <Checkbox colorScheme="pink" defaultChecked mt={2}>
+              Default
+            </Checkbox>
+            <Checkbox colorScheme="pink" defaultChecked mt={2}>
+              Female
+            </Checkbox>
+            <Checkbox colorScheme="pink" defaultChecked mt={2}>
+              Non-bunary people
+            </Checkbox>
+          </div>
+          <div className="mt-[60px]">
+            <p>Age Range</p>
+            <SliderAge />
+          </div>
+        </div>
       </div>
     </>
   );
