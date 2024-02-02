f4746906a1e7738e2e3877cd0cac23cc967cff63
Kaew <rajitkaew2285@gmail.com>
2024-02-02 14:36:27 +0700

    feat:add props to  navbar

--- client/src/components/common/ChakraButton.jsx
+++ client/src/components/common/ChakraButton.jsx
@@ -5,13 +5,14 @@ function ChakraButton(props) {
   const handleNext = () => {
     props.onClick();
   };
+  const combinedClassName = `${props.width} ${props.height} hover:cursor-pointer`;
 
   return (
     <Button
       onClick={handleNext}
       m={10}
       colorScheme={props.color}
-      className="w-[150px] hover:cursor-pointer"
+      className={combinedClassName}
     >
       {props.name}
     </Button>
@@ -21,5 +22,7 @@ ChakraButton.propTypes = {
   color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
 };
 export default ChakraButton;
