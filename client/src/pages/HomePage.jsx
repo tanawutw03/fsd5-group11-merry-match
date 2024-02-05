4623644a00a76264a30222452b2c3178f979784f
Tanawut Wongboot <tanawut.w@outlook.com>
2024-02-05 14:47:13 +0700

    fix(user-auth): save image to localstorage to preserve avatarurl

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,13 +3,20 @@ import { useUser } from "../app/userContext.js";
 import { useEffect } from "react";
 
 function HomePage() {
-  const { user, avatarUrl } = useUser();
+  const { user, setUser, avatarUrl, setAvatarUrl } = useUser();
 
   useEffect(() => {
-    console.log(`avatar from homepage `, avatarUrl);
+    console.log(`avtarUrl in HomePage `, avatarUrl);
   }, [avatarUrl]);
 
-  return <NonUserHomePage user={user} avatarUrl={avatarUrl} />;
+  return (
+    <NonUserHomePage
+      user={user}
+      setUser={setUser}
+      avatarUrl={avatarUrl}
+      setAvatarUrl={setAvatarUrl}
+    />
+  );
 }
 
 export default HomePage;
