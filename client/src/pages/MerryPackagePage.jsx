5214ee2bc3229c28c0939311e2c24c9b81600bba
devsomsak <devrleang@gmail.com>
2024-02-05 16:49:18 +0700

    feat:update merry-match-page

--- client/src/pages/MerryPackagePage.jsx
+++ client/src/pages/MerryPackagePage.jsx
@@ -1,15 +1,33 @@
 import logo from "../assets/merryPackagePage/logo.svg";
 import bell from "../assets/merryPackagePage/frame.svg";
-import basic from "../assets/merryPackagePage/basic.svg";
 import fillCheckbox from "../assets/merryPackagePage/checkbox-circle-fill.svg";
-import platinum from "../assets/merryPackagePage/platinum.svg";
-import premium from "../assets/merryPackagePage/premium.svg";
 import person01 from "../assets/merryPackagePage/person01.png";
 import facebookIcon from "../assets/merryPackagePage/facebook-circle-fill.svg";
 import instagramIcon from "../assets/merryPackagePage/instagram-fill.svg";
 import twitterIcon from "../assets/merryPackagePage/twitter-fill.svg";
+import { supabase } from "../utils/supabaseClient";
+import { useState, useEffect } from "react";
 
 function PackagePage() {
+  const [packages, setPackages] = useState([]);
+
+  useEffect(() => {
+    getPackages();
+  }, []);
+
+  async function getPackages() {
+    try {
+      const { data, error } = await supabase.from("packages").select("*");
+      // .limmit(3);
+      if (error) throw error;
+      if (data != null) {
+        setPackages(data);
+      }
+      console.log(packages);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <>
       <div className="main-container w-screen  p-[80px,161px,160px,160px]">
@@ -64,159 +82,60 @@ function PackagePage() {
             </div>
 
             <div className="flex justify-center items-center gap-[24px] mt-[80px]">
-              <div className="basic-package-con flex flex-col items-start w-[357px] p-[40px] gap-[24px] rounded-[32px] border-[1px] border-solid border-gray-400 bg-[#FFF]">
-                <ul>
-                  <div className="basic-package">
-                    <li className="card-icon mb-[24px]">
-                      <img
-                        src={basic}
-                        alt=""
-                        className="w-[36px] h-[36px] flex-shrink-0"
-                      />
-                    </li>
-                    <li className="card-category font-nunito text-[32px] font-bold text-purple-800">
-                      Basic
-                    </li>
-                    <li className="card-paid-condition mb-[24px]">
-                      <span className="card-package-price font-nunito text-[20px] font-bold text-gray-900">
-                        THB 59.00
-                      </span>
-                      <span className="card-package-period font-nunito text-[16px] font-bold text-gray-600">
-                        /month
-                      </span>
-                    </li>
+              {packages.map((packages, index) => (
+                <div className="basic-package-con flex flex-col items-start w-[357px] p-[40px] gap-[24px] rounded-[32px] border-[1px] border-solid border-gray-400 bg-[#FFF]">
+                  <ul key={index}>
+                    <div className="basic-package">
+                      <li className="card-icon mb-[24px]">
+                        <img
+                          src={packages.iconurl}
+                          alt=""
+                          className="w-[36px] h-[36px] flex-shrink-0"
+                        />
+                      </li>
+                      <li className="card-category font-nunito text-[32px] font-bold text-purple-800">
+                        {packages.name}
+                      </li>
+                      <li className="card-paid-condition mb-[24px]">
+                        <span className="card-package-price font-nunito text-[20px] font-bold text-gray-900">
+                          THB {packages.price} /month
+                        </span>
+                      </li>
 
-                    <li className="fill-checkbox-icon">
-                      <div className="flex">
-                        <img src={fillCheckbox} alt="" />
-                        <p>‘Merry’ more than a daily limited </p>
-                      </div>
-                      <div className="flex pb-[36px]">
-                        <img src={fillCheckbox} alt="" />
-                        <p>Up to 25 Merry per day</p>
-                      </div>
-                    </li>
-                    <button
-                      className="choose-package-btn flex justify-center items-center gap-[8px] w-[277px]   
-                           rounded-[99px]  bg-red-100  hover:bg-red-200 active:bg-red-300  shadow-setShadow01"
-                      type="button"
-                      disabled
-                    >
-                      <label
-                        className="font-nunito text-center text-[16px] font-bold text-red-600 p-[12px,24px]  "
-                        htmlFor="ChoosePackageSection"
+                      <li className="fill-checkbox-icon">
+                        <div className="flex">
+                          <img src={fillCheckbox} alt="" />
+                          <p>‘Merry’ more than a daily limited </p>
+                        </div>
+                        <div className="flex pb-[36px]">
+                          <img src={fillCheckbox} alt="" />
+                          <p>Up to {packages.merry_limit} Merry per day</p>
+                        </div>
+                      </li>
+                      <button
+                        className="choose-package-btn flex justify-center items-center gap-[8px] w-[277px]   
+                           rounded-[99px]  bg-red-100  hover:bg-red-200 active:bg-red-300  shadow-setShadow01 h-12 p-[16px]"
+                        type="button"
+                        disabled
                       >
-                        Choose Package
-                      </label>
-                    </button>
-                  </div>
-                </ul>
-              </div>
-
-              <div className="platinum-package-con flex flex-col items-start w-[357px] p-[40px] gap-[24px] rounded-[32px] border-[1px] border-solid border-gray-400 bg-[#FFF]">
-                <ul>
-                  <div className="paltinum-package">
-                    <li className="card-icon mb-[24px]">
-                      <img
-                        src={platinum}
-                        alt=""
-                        className="w-[36px] h-[36px] flex-shrink-0"
-                      />
-                    </li>
-                    <li className="card-category font-nunito text-[32px] font-bold text-purple-800">
-                      Paltinum
-                    </li>
-                    <li className="card-paid-condition mb-[24px]">
-                      <span className="card-package-price font-nunito text-[20px] font-bold text-gray-900">
-                        THB 99.00
-                      </span>
-                      <span className="card-package-period font-nunito text-[16px] font-bold text-gray-600">
-                        /month
-                      </span>
-                    </li>
-
-                    <li className="fill-checkbox-icon">
-                      <div className="flex">
-                        <img src={fillCheckbox} alt="" />
-                        <p>‘Merry’ more than a daily limited </p>
-                      </div>
-                      <div className="flex pb-[36px]">
-                        <img src={fillCheckbox} alt="" />
-                        <p>Up to 45 Merry per day</p>
-                      </div>
-                    </li>
-                    <button
-                      className="choose-package-btn flex justify-center items-center gap-[8px] w-[277px]  p-[12px,24px]  
-                           rounded-[99px]  bg-red-100  hover:bg-red-200 active:bg-red-300  shadow-setShadow01"
-                      type="button"
-                      disabled
-                    >
-                      <label
-                        className="font-nunito text-center text-[16px] font-bold text-red-600 leading-6"
-                        htmlFor="ChoosePackageSection"
-                      >
-                        Choose Package
-                      </label>
-                    </button>
-                  </div>
-                </ul>
-              </div>
-
-              <div className="premium-package-con flex flex-col items-start w-[357px] p-[40px] gap-[24px] rounded-[32px] border-[1px] border-solid border-gray-400 bg-[#FFF]">
-                <ul>
-                  <div className="premium-package">
-                    <li className="card-icon mb-[24px]">
-                      <img
-                        src={premium}
-                        alt=""
-                        className="w-[36px] h-[36px] flex-shrink-0"
-                      />
-                    </li>
-                    <li className="card-category font-nunito text-[32px] font-bold text-purple-800">
-                      Premium
-                    </li>
-                    <li className="card-paid-condition mb-[24px]">
-                      <span className="card-package-price font-nunito text-[20px] font-bold text-gray-900">
-                        THB 149.00
-                      </span>
-                      <span className="card-package-period font-nunito text-[16px] font-bold text-gray-600">
-                        /month
-                      </span>
-                    </li>
-
-                    <li className="fill-checkbox-icon">
-                      <div className="flex">
-                        <img src={fillCheckbox} alt="" />
-                        <p>‘Merry’ more than a daily limited </p>
-                      </div>
-                      <div className="flex pb-[36px]">
-                        <img src={fillCheckbox} alt="" />
-                        <p>Up to 70 Merry per day</p>
-                      </div>
-                    </li>
-                    <button
-                      className="choose-package-btn flex justify-center items-center gap-[8px] w-[277px]  p-[12px,24px]  
-                           rounded-[99px]  bg-red-100  hover:bg-red-200 active:bg-red-300  shadow-setShadow01"
-                      type="button"
-                      disabled
-                    >
-                      <label
-                        className="font-nunito text-center text-[16px] font-bold text-red-600   "
-                        htmlFor="ChoosePackageSection"
-                      >
-                        Choose Package
-                      </label>
-                    </button>
-                  </div>
-                </ul>
-              </div>
+                        <label
+                          className="font-nunito text-center text-[16px] font-bold text-red-600 p-[12px,24px]  "
+                          htmlFor="ChoosePackageSection"
+                        >
+                          Choose Package
+                        </label>
+                      </button>
+                    </div>
+                  </ul>
+                </div>
+              ))}
             </div>
           </div>
         </section>
 
         <footer>
           <div className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-[1440px] h-[371px] p-[48px,160px] bg-gray-100">
+            <div className="flex justify-center items-center w-screen h-[371px] p-[48px,160px] bg-gray-100">
               <ul className="flex  flex-col justify-center items-center w-[1120px] h-[275px] flex-shrink-0">
                 <li className="merry-match-logo ">
                   <img src={logo} />
