0020b241e1364c9246c550cd31bc2886d47c6099
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-02-29 10:28:30 +0700

    fix:correct lasybill

--- client/src/pages/UserMembership.jsx
+++ client/src/pages/UserMembership.jsx
@@ -284,7 +284,7 @@ function UserMembership() {
               <div className="PackageCard  px-8 pt-8 pb-6 bg-white rounded-[32px] border border-gray-300 flex-col justify-start items-end gap-4 flex">
                 <div className="Table w-[866px] py-2 border-b border-gray-200 justify-start items-start gap-4 inline-flex">
                   <div className="NextBilling01092022 grow shrink basis-0 text-slate-500 text-xl font-semibold font-['Nunito'] leading-[30px]">
-                    Next billing : {lastBill ? lasybill : "No Packages"}
+                    Next billing : {lastBill ? lastbill : "No Packages"}
                   </div>
                 </div>
                 <div className="BillingDetail pb-6 border-b border-gray-200 flex-col justify-start items-start flex">
