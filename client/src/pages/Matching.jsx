ffa48741d36bbf0f56bdb1c2d63093d7f736395a
Kaew <rajitkaew2285@gmail.com>
2024-02-29 10:18:22 +0700

    fix(Matching) edit bug Chatroom

--- client/src/pages/Matching.jsx
+++ client/src/pages/Matching.jsx
@@ -53,41 +53,51 @@ function Matching() {
         />
 
         <div className="flex flex-row w-full  m-0 p-0 ">
-          <LeftSideMatching mutualMatch={mutualMatch} />
-          <MerryCards user={user} onMutualMatch={handleMutualMatch} />
-          <div className="  flex-col flex w-1/6  p-6  ">
-            <p className=" text-[#191C77] text-base font-bold">
-              Sex you interest
-            </p>
-            <div className="mt-2 flex flex-col">
-              <Checkbox colorScheme="pink" defaultChecked mt={2}>
-                Default
-              </Checkbox>
-              <Checkbox colorScheme="pink" defaultChecked mt={2}>
-                Female
-              </Checkbox>
-              <Checkbox colorScheme="pink" defaultChecked mt={2}>
-                Non-bunary people
-              </Checkbox>
-            </div>
-            <SliderAge />
-            <div className="flex flex-col pl-5 mt-40 border-t border-solid border-t-[color:var(--gray-300,#E4E6ED)] ">
-              <div className="flex gap-5 justify-between pl-6">
-                <ChakraButton
-                  name=" Clear"
-                  colorScheme="red"
-                  variant="ghost"
-                  rounded="20px"
-                ></ChakraButton>
-                <ChakraButton
-                  name=" Search"
-                  colorScheme="red"
-                  variant="solid"
-                  rounded="20px"
-                ></ChakraButton>
+          <LeftSideMatching
+            mutualMatch={mutualMatch}
+            onMutualMatchClick={toggleChatroom}
+          />
+
+          {showChatroom ? (
+            <Chatroom profile={selectedProfile} user={user} />
+          ) : (
+            <>
+              <MerryCards user={user} onMutualMatch={handleMutualMatch} />
+              <div className="  flex-col flex w-1/6  p-6  ">
+                <p className=" text-[#191C77] text-base font-bold">
+                  Sex you interest
+                </p>
+                <div className="mt-2 flex flex-col">
+                  <Checkbox colorScheme="pink" defaultChecked mt={2}>
+                    Default
+                  </Checkbox>
+                  <Checkbox colorScheme="pink" defaultChecked mt={2}>
+                    Female
+                  </Checkbox>
+                  <Checkbox colorScheme="pink" defaultChecked mt={2}>
+                    Non-bunary people
+                  </Checkbox>
+                </div>
+                <SliderAge />
+                <div className="flex flex-col pl-5 mt-40 border-t border-solid border-t-[color:var(--gray-300,#E4E6ED)] ">
+                  <div className="flex gap-5 justify-between pl-6">
+                    <ChakraButton
+                      name=" Clear"
+                      colorScheme="red"
+                      variant="ghost"
+                      rounded="20px"
+                    ></ChakraButton>
+                    <ChakraButton
+                      name=" Search"
+                      colorScheme="red"
+                      variant="solid"
+                      rounded="20px"
+                    ></ChakraButton>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </>
