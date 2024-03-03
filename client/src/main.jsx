1aaa31ba11bed12b73ce7393ecd8abbc72e97eb3
Tanawut Wongboot <tanawut.w@outlook.com>
2024-03-03 13:36:06 +0700

    fix(main): correct app.jsx to App.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
+import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { UserProvider } from "./app/userContext.js";
